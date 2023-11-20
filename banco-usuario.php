c46d3fa8b9bc077f5c290a0e533bc1f3d1c8c950
carllos39 <carllos2005@outlook.com.br>
2023-11-20 15:58:49 -0300

    atualizar

--- banco-usuario.php
+++ banco-usuario.php
@@ -4,8 +4,8 @@ include("conecta.php");
 
 <?php
 function buscaUsuario($conexao,$email,$senha) {
-     $senhaMd5=md5($senha);
-$query="select * from usuarios where email='{$email}' and senha='{$senhaMd5}'";
+     
+$query="select * from usuarios where email='{$email}' and senha='{$senha}'";
 $resultado=mysqli_query($conexao,$query);
 $usuario=mysqli_fetch_assoc($resultado);
 return $usuario;
